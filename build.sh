5b68d5c4e1afcc9a0a0db505274d3904fed745e4
Ryan Voots <simcop2387@simcop2387.info>
2017-10-14 15:39:26 -0700

    Need to add -lssl but it builds on my system

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@
 set -e
 set -u
 
+# Clear the build area
 rm -rf build/
 mkdir build/
 
+# Prep the DBD-SQLite repo
 pushd DBD-SQLite
 git checkout master # TODO make env variable
 git clean -f -x
@@ -13,22 +15,26 @@ git reset --hard HEAD
 git pull
 popd
 
+# Prep the build area
 rsync -vax DBD-SQLite/* build/
 
 pushd build
 patch -p1 -F10 < ../patches/01-add-sqlcihper-defines.patch
 patch -p1 -F10 < ../patches/02-add-ext-includes.patch
+
+# find ./ \( -name MANIFEST -or -iname \*.pm -or -iname \*.pl -or -iname \*.pod -or -iname \*.t \) -exec perl -pi -E 's{DBD(::|-|/)SQLite}{DBD$1SQLcipher}g;' {} +
+#find ./lib -type f -iname \*sqlite\* -exec rename 's/SQLite/SQLcipher/g; s/sqlite/sqlcipher/g;' {} \;
+#mv lib/DBD/SQLite lib/DBD/SQLcipher
+
 popd
 
+
+# Prep and configure the sqlcipher repo
 pushd sqlcipher
 git checkout master # TODO make env variable
 git clean -f -x
 git reset --hard HEAD
 git pull
-
-
-# Build options and such stolen from debian's source package for SQLcipher
-export CFLAGS="-O2 -fno-strict-aliasing -DSQLITE_SECURE_DELETE -DSQLITE_ENABLE_COLUMN_METADATA -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_RTREE=1 -DSQLITE_SOUNDEX=1 -DSQLITE_ENABLE_UNLOCK_NOTIFY -DSQLITE_OMIT_LOOKASIDE=1 -DSQLITE_ENABLE_UPDATE_DELETE_LIMIT=1 -DSQLITE_MAX_SCHEMA_RETRY=25 -DSQLITE_HAS_CODEC -DHAVE_USLEEP=1"
 
 patch -p1 < ../debianpatches/20-change-name-to-sqlcipher.patch
 patch -p1 < ../debianpatches/33-add-have-usleep.patch
@@ -38,9 +44,10 @@ patch -p1 < ../debianpatches/33-add-have-usleep.patch
 
 make sqlite3.c
 
-cp -av sqlite* fts* ext ../build
+cp -av sqlite* ext ../build
 popd
-find build/ -iname \*.pm -or -iname \*.pl -or -iname \*.pod -or -iname \*.t -exec perl -i.bak -E 's/DBD(::|-)SQLite/DBD$1SQLcipher/g;' {} +
+
+# Try to actually build and test
 
 pushd build/
 perl Makefile.PL
