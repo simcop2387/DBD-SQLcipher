25ab83fa32463bcfaaf4c3d4ecd21e60dcc72586
Ryan Voots <simcop2387@simcop2387.info>
2017-10-14 14:52:57 -0700

    Getting there, still doesnt build

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is an experimental project to build DBD::SQLcipher based on the DBD-SQLite and SQLcipher codebases
+
+debianpatches/ are blatently stolen from the debian source package for SQLcipher 3.4.1
--- build.sh
+++ build.sh
@@ -9,9 +9,20 @@ mkdir build/
 rsync -vax DBD-SQLite/* build/
 
 pushd sqlcipher
-git clean -f
+git clean -f -x
 git reset --hard HEAD
 git pull
+
+
+# Build options and such stolen from debian's source package for SQLcipher
+export CFLAGS="-O2 -fno-strict-aliasing -DSQLITE_SECURE_DELETE -DSQLITE_ENABLE_COLUMN_METADATA -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_RTREE=1 -DSQLITE_SOUNDEX=1 -DSQLITE_ENABLE_UNLOCK_NOTIFY -DSQLITE_OMIT_LOOKASIDE=1 -DSQLITE_ENABLE_UPDATE_DELETE_LIMIT=1 -DSQLITE_MAX_SCHEMA_RETRY=25 -DSQLITE_HAS_CODEC -DHAVE_USLEEP=1"
+
+patch -p1 < ../debianpatches/20-change-name-to-sqlcipher.patch
+patch -p1 < ../debianpatches/33-add-have-usleep.patch
+
+./configure --enable-threadsafe --enable-load-extension --enable-tempstore --disable-tcl --disable-fts3 --disable-fts4 --disable-fts5
+
+
 make sqlite3.c
 
 cp -av sqlite* ../build
