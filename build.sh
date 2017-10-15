7f03846a77969125faef2e10d499be3f915a8a01
Ryan Voots <simcop2387@simcop2387.info>
2017-10-14 17:27:37 -0700

    Builds now with the correct name

--- build.sh
+++ build.sh
@@ -24,9 +24,11 @@ for patch in ../patches/*.patch; do
 	patch -p1 -F10 < $patch
 done
 
-# find ./ \( -name MANIFEST -or -iname \*.pm -or -iname \*.pl -or -iname \*.pod -or -iname \*.t \) -exec perl -pi -E 's{DBD(::|-|/)SQLite}{DBD$1SQLcipher}g;' {} +
-#find ./lib -type f -iname \*sqlite\* -exec rename 's/SQLite/SQLcipher/g; s/sqlite/sqlcipher/g;' {} \;
-#mv lib/DBD/SQLite lib/DBD/SQLcipher
+find ./ \( -name MANIFEST -or -name constants.inc -or -iname \*.pm -or -iname \*.pl -or -iname \*.pod -or -iname \*.t -or -iname \*.xs \) -exec perl -pi -E 's{DBD(::|-|/)SQLite}{DBD$1SQLcipher}g; s/dbi:SQLite/dbi:SQLcipher/g' {} +
+find ./lib -type f -iname \*sqlite\* -exec rename 's/SQLite/SQLcipher/g; s/sqlite/sqlcipher/g;' {} \;
+mv lib/DBD/SQLite lib/DBD/SQLcipher
+mv SQLite.xs SQLcipher.xs
+perl -i -pE 's/SQLite.xsi/SQLcipher.xsi/g;' SQLcipher.xs
 
 popd
 
